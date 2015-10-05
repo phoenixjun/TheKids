ad8a00444c61c25fe0445a33836bdea7fe57bc76
phoenixjun <phoenix_jun@hotmail.com>
2015-10-05 16:51:24 +1100

    implement tutorial step 8 https://docs.angularjs.org/tutorial/step_08

--- TheKids.Website/Scripts/app/phones/thekids-phonedetails-controller.js
+++ TheKids.Website/Scripts/app/phones/thekids-phonedetails-controller.js
@@ -1,10 +1,20 @@
 ﻿'use strict';
 (function() {
 
-    var app = angular.module("thekidsphone");
-    var phoneCtrl = function ($routeParams) {
+    var app = angular.module("thekidsphone",[]);
+    var phoneCtrl = function($routeParams, $http) {
         var vm = this;
         vm.phoneId = $routeParams.phoneId;
+
+        $http.get('/content/phones/' + vm.phoneId + '.json')
+            .success(function(data) {
+                vm.phone = data;
+                var imgs = new Array();
+                angular.forEach(vm.phone.images, function(value, key) {
+                    imgs.push('/content/' + value);
+                });
+                vm.phone.images = imgs;
+            });
     }
-    app.controller('thekids-phonedetails-controller', ['$routeParams', phoneCtrl]);
+    app.controller('thekids-phonedetails-controller', ['$routeParams', '$http', phoneCtrl]);
 }());
